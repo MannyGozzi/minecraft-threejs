14f2f6d7cc376dec62b87392f6ca9637a05b9b41
Glitch (hello-express) <none>
2019-10-11 15:27:26 +0000

    👙🚋 Checkpoint ./package.json:2579398/1278 ./views/index.html:2579398/1822 ./views/index.pug:2579398/217 ./views/includes/defaults.pug:2579398/465 ./server.js:2579398/98 ./public/client.js:2579398/1169 ./public/style.css:2579398/1005 ./.glitch-assets:2579398/444

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const app = express();
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
+app.set('view engine', 'pug');
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+  response.render('index');
 });
 
 // listen for requests :)
