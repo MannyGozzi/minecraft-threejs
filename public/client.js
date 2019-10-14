362827e64b318e280b94999163499c5079aa2fce
Glitch (hello-express) <none>
2019-10-14 05:31:13 +0000

    🏢👼 Checkpoint ./public/client.js:2579398/3

--- public/client.js
+++ public/client.js
@@ -14,7 +14,7 @@ const near = 0.1; // the near clipping plane
 const far = 100; // the far clipping plane
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 // we'll move the camera back a bit so that we can view the scene
-camera.position.set(0, 0, 10);
+camera.position.set(0, 0, 5);
 
 //initialize the renderer
 const renderer = new THREE.WebGLRenderer({
