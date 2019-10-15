c200e81433a10d2ee4a241c7a1c15eb9b79d2679
Glitch (threejs-demo-practice) <none>
2019-10-15 18:20:40 +0000

    👸🛰 Checkpoint ./public/style.css:2579398/259 ./public/client.js:2579398/1

--- public/client.js
+++ public/client.js
@@ -13,7 +13,7 @@ scene.background = new THREE.Color("lightblue");
 const fov = 90; // AKA Field of View
 const aspect = window.innerWidth / window.innerHeight;
 const near = 0.1; // the near clipping plane
-const far = 1000; // the far clipping plane
+const far = 100; // the far clipping plane
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 // we'll move the camera back a bit so that we can view the scene
 camera.position.set(0, 0, 5);
