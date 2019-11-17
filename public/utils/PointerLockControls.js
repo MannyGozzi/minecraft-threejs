7d32efe8fbad714b8953166783bcf84d1afa82b9
Glitch (threejs-demo-practice) <none>
2019-11-17 18:31:39 +0000

    💧🎐 Checkpoint ./public/utils/PointerLockControls.js:2579398/4078

--- public/utils/PointerLockControls.js
+++ public/utils/PointerLockControls.js
@@ -1,3 +1,5 @@
+// TODO look at chrome dev tools to see why physics is super slow as time progresses
+
 // this is the largest and most complicated part of the code
 // it can be modulized with great effort though
 export default class PointerLockControls {
@@ -287,7 +289,7 @@ export default class PointerLockControls {
   
   cleanCollisionObjects() {
     this.objects.filter(object => {
-      if (object.position.distanceTo(this.object.position) > 2) {
+      if (object.position.distanceTo(this.object.position) > 3) {
         this.scene.remove(object);
         return true;
       }
