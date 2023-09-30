16fa190bfd1fc0664aac8916664aa9697e53c06a
Glitch (threejs-demo-practice) <none>
2023-09-30 19:45:24 +0000

    🎀🎷 Checkpoint ./server.js:2579398/192 ./package.json:2579398/1381 ./public/index.html:2579398/661 ./src/index.html:2579398/254

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-// server.js
-// where your node app starts
 
-// init project
 const express = require("express");
 const app = express();
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.render('index');
 });
 
-// listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
